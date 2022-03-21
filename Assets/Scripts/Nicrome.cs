9ac62656653b68da9952a1d55f88d0ad9c48e7ec
momo990305@gmail.com <momo990305@gmail.com>
2022-03-21 20:34:32 +0900

    until the generation of nichrome wires

---
+++ Assets/Scripts/Nicrome.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScripts : MonoBehaviour
+{
+    int element = 0; // sample-coated element number - if it is 0, nothing.
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
