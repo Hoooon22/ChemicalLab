74e49abf64ed49f4d65370f9791ce51262bbf0dd
momo990305@gmail.com <momo990305@gmail.com>
2022-03-21 20:53:31 +0900

    add element variable

--- Assets/Scripts/Nicrome.cs
+++ Assets/Scripts/Nicrome.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SampleScripts : MonoBehaviour
+public class Nicrome : MonoBehaviour
 {
     int element = 0; // sample-coated element number - if it is 0, nothing.
 
